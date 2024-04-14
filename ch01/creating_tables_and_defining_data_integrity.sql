fe7136556d7eb1938048cc1b5e14aa5d7dd3124d
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-04-14 10:01:56 +0800

    add DDL basics script

--- ch01/creating_tables_and_defining_data_integrity.sql
+++ ch01/creating_tables_and_defining_data_integrity.sql
@@ -1,3 +1,5 @@
+-- Creating tables
+
 USE TSQLV6;
 
 DROP TABLE IF EXISTS dbo.Employees;
@@ -12,3 +14,51 @@ CREATE TABLE dbo.Employees
     ssn       VARCHAR(20) NOT NULL,
     salary    MONEY       NOT NULL
 );
+
+-- Defining data integrity
+
+-- Primary key constraints
+
+ALTER TABLE dbo.Employees
+    ADD CONSTRAINT PK_Employees
+    PRIMARY KEY(empid);
+
+-- Uniqueness constraints
+ALTER TABLE dbo.Employees
+    ADD CONSTRAINT UNQ_Employees_ssn
+    UNIQUE(ssn);
+
+-- "Uniqueness constraint" for non-null values only
+CREATE UNIQUE INDEX idx_ssn_notnull ON dbo.Employees(ssn) WHERE ssn IS NOT NULL;
+
+-- Foreign key constraints
+
+DROP TABLE IF EXISTS dbo.Orders;
+
+CREATE TABLE dbo.Orders
+(
+    orderid    INT         NOT NULL,
+    empid      INT         NOT NULL,
+    custid     VARCHAR(10) NOT NULL
+    orderts    DATETIME2   NOT NULL,
+    qty        INT         NOT NULL,
+    CONSTRAINT PK_Orders
+        PRIMARY KEY(orderid)
+);
+
+ALTER TABLE dbo.Orders
+    ADD CONSTRAINT FK_Orders_Employees
+    FOREIGN KEY(empid)
+    REFERENCES dbo.Employees(empid);
+
+-- Check contraints
+
+ALTER TABLE dbo.Employees
+    ADD CONSTRAINT CHK_Employees_salary
+    CHECK(salary > 0.00);
+
+-- Default constraints
+
+ALTER TABLE dbo.Orders
+    ADD CONSTRAINT DFT_Orders_orders
+    DEFAULT(SYSDATETIME()) FOR ORDERS;
